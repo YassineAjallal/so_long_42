163281348b7ed666c57ffb06ab1992bc639dbd70
yassine542 <101680065+yassine542@users.noreply.github.com>
2023-02-05 14:22:56 +0100

    learn

--- window.c
+++ window.c
@@ -6,7 +6,7 @@
 /*   By: yajallal < yajallal@student.1337.ma >      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/05 00:12:46 by yajallal          #+#    #+#             */
-/*   Updated: 2023/02/05 00:55:52 by yajallal         ###   ########.fr       */
+/*   Updated: 2023/02/05 14:14:20 by yajallal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,14 +28,16 @@ void ft_wall(char **map, void *mlx, void *mlx_win)
 {
 	void *wall;
 	void *pacman;
+	void *coin;
 	int		img_width;
 	int		img_height;
 	int 	i;
 	int 	j;
 	
 	i = 0;
-	wall = mlx_xpm_file_to_image(mlx, "./image/wall.xpm", &img_width, &img_height);
-	pacman = mlx_xpm_file_to_image(mlx, "./image/Pacman.xpm", &img_width, &img_height);
+	wall = mlx_xpm_file_to_image(mlx, "./image/walls.xpm", &img_width, &img_height);
+	pacman = mlx_xpm_file_to_image(mlx, "./image/zombie.xpm", &img_width, &img_height);
+	coin = mlx_xpm_file_to_image(mlx, "./image/food.xpm", &img_width, &img_height);
 	while (map[i])
 	{
 		j = 0;
@@ -45,6 +47,8 @@ void ft_wall(char **map, void *mlx, void *mlx_win)
 				mlx_put_image_to_window(mlx, mlx_win, wall, j * 50, i * 50);
 			else if (map[i][j] == 'P')
 				mlx_put_image_to_window(mlx, mlx_win, pacman, j * 50, i * 50);
+			else if (map[i][j] == 'C')
+				mlx_put_image_to_window(mlx, mlx_win, coin, j * 50, i * 50);
 			j++;
 		}
 		i++;
